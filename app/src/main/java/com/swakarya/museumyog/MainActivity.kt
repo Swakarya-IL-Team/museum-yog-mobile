b529f564720975bc5bb42a74229327ec95448244
Ramadani354 <yoviadhi123@gmail.com>
2024-05-20 18:08:42 +0700

    Add Profile Menu

--- app/src/main/java/com/swakarya/museumyog/MainActivity.kt
+++ app/src/main/java/com/swakarya/museumyog/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.swakarya.museumyog.navigation.Navigation
+import com.swakarya.museumyog.presentation.ProfileMenu.ProfileMenu
 import com.swakarya.museumyog.ui.theme.MuseumyogmobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +13,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MuseumyogmobileTheme {
                 Navigation()
+                ProfileMenu()
             }
         }
     }
--- app/src/main/java/com/swakarya/museumyog/navigation/navigation.kt
+++ app/src/main/java/com/swakarya/museumyog/navigation/navigation.kt
@@ -54,5 +54,7 @@ fun Navigation() {
                 navController = navController
             )
         }
+
+
     }
 }
